327bfc8ef77a95ab7679a1e954a044f27e0d2a79
murad <muradkv@gmail.com>
2025-08-30 17:44:10 +0300

    Initial commit

---
+++ Apple-Frameworks_SUI/Apple_Frameworks_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_SUIApp.swift
+//  Apple-Frameworks_SUI
+//
+//  Created by murad on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
