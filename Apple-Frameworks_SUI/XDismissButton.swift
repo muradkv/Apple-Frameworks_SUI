10458b2091d0ab4258a3586e67229dc870134038
murad <muradkv@gmail.com>
2025-09-04 21:39:11 +0300

    Refact button into separate XDismissButton view file
    Refactored the button in FrameworkDetailView into a separate reusable AFButton view in its own file.
    Improved code modularity and maintainability by isolating button styling and behavior.
    Updated FrameworkDetailView to use the new AFButton component.

---
+++ Apple-Frameworks_SUI/XDismissButton.swift
@@ -0,0 +1,32 @@
+//
+//  XDismissButton.swift
+//  Apple-Frameworks_SUI
+//
+//  Created by murad on 04.09.2025.
+//
+
+import SwiftUI
+
+struct XDismissButton: View {
+    @Binding var isShowingDetailView: Bool
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                isShowingDetailView = false
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(Color(.label))
+                    .imageScale(.large)
+                    .frame(width: 44, height: 44)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    XDismissButton(isShowingDetailView: .constant(false))
+}
