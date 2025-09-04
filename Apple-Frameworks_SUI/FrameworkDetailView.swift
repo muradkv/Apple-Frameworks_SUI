d0fd853dc83a574877e4954acbd5b1f2dba51c0e
murad <muradkv@gmail.com>
2025-09-04 21:15:20 +0300

    Add SafariView with SFSafariViewController and button integration
    Implemented SafariView struct using UIViewControllerRepresentable to integrate SFSafariViewController in SwiftUI.
    Added button in FrameworkDetailView to trigger SafariView for displaying web content. Ensured seamless navigation and dismissal for web page viewing.

--- Apple-Frameworks_SUI/FrameworkDetailView.swift
+++ Apple-Frameworks_SUI/FrameworkDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FrameworkDetailView: View {
     var framework: Framework
     @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     
     var body: some View {
         VStack {
@@ -38,10 +39,15 @@ struct FrameworkDetailView: View {
             
             Button {
                 print("tapped")
+                isShowingSafariView = true
             } label: {
                 AFButton(title: "Learn More")
             }
-
+        }
+        .sheet(isPresented: $isShowingSafariView) {
+            SafariView(
+                url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!
+            )
         }
     }
 }
---
+++ Apple-Frameworks_SUI/SafariView.swift
@@ -0,0 +1,24 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks_SUI
+//
+//  Created by murad on 04.09.2025.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(
+        _ uiViewController: SFSafariViewController,
+        context: UIViewControllerRepresentableContext<SafariView>
+    ) {
+        
+    }
+}
